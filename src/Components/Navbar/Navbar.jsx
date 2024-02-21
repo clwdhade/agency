4c52ea61f368bfa71e4362adb50e09b0488538a8
octocatblain <blainmuema@gmail.com>
2024-02-21 22:33:41 +0300

    remove  useless css classes
    Co-authored-by: octocatblain <blainmuema@gmail.com>
    Co-authored-by: clwdhade-admin <clwdhade@gmail.com>

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
         <Navbar className="flex flex-row justify-between px-12 py-1 bg-blur-lg text-white">
             <div className="logo">Agency</div>
             <ul className='flex flex-row gap-2 text-2xl'>
-                <li className='font-bold text-blue-700'>Home</li>
-                <li className='font-bold text-blue-700'>Services</li>
-                <li className='font-bold text-blue-700'>About</li>
-                <li className='font-bold text-blue-700'>Companies</li>
-                <li className='font-bold text-blue-700'>Contact</li>
+                <li >Home</li>
+                <li >Services</li>
+                <li >About</li>
+                <li >Companies</li>
+                <li >Contact</li>
 
             </ul>
         </Navbar>
