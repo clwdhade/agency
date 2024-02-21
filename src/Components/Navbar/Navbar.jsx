7c915f0f741b193873d00db41e4c692fe151d5f5
octocatblain <blainmuema@gmail.com>
2024-02-21 22:20:46 +0300

    feat: add new tailwind class to menu item
    Co-authored-by: BLAIN <blainmuema@gmail.com>
    Co-authored-by: CLWDHADE ADMIN <clwdhade@gmail.com>

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,8 +5,7 @@ const Navbar = () => {
         <Navbar className="flex flex-row justify-between px-12 py-1 bg-blur-lg text-white">
             <div className="logo">Agency</div>
             <ul className='flex flex-row gap-2 text-2xl'>
-                <li>Home</li>
-
+                <li className='font-bold text-blue-700'>Home</li>
                 <li>Services</li>
                 <li>About</li>
                 <li>Companies</li>
