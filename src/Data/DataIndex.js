cc58ae9e7c9bb9f76caf9caf8f04a3c3a0d5dbad
octocatblain <blainmuema@gmail.com>
2024-02-24 21:23:56 +0300

    feat: add icons to service cards

--- src/Data/DataIndex.js
+++ src/Data/DataIndex.js
@@ -1,3 +1,10 @@
+import cv from "../assets/images/cv.png"
+import career from "../assets/images/career.png"
+import employment from "../assets/images/employment.png"
+import interview from "../assets/images/interview.png"
+import jobsearch from "../assets/images/jobsearch.png"
+
+
 export const HomeHeroImages = [
     {
         id: "1",
@@ -36,22 +43,27 @@ export const HomeHeroImages = [
 
 export const Services = [
     {
+        Image: jobsearch,
         Service: "Job Placement",
         Description: "Our expert recruiters specialize in matching talented individuals with the perfect career opportunities across various industries. Whether you're seeking entry-level positions or executive roles, we're here to guide you every step of the way."
     },
     {
+        Image: cv,
         Service: "Resume Optimization",
         Description: "Stand out from the crowd with a professionally crafted resume tailored to your skills and experience. Our team will work closely with you to highlight your strengths and accomplishments, increasing your chances of landing interviews."
     },
     {
+        Image: interview,
         Service: "Interview Preparation",
         Description: "Ace your interviews with confidence! Through personalized coaching sessions, mock interviews, and valuable tips, we'll equip you with the skills and strategies needed to impress hiring managers and secure your dream job."
     },
     {
+        Image: career,
         Service: "Career Counseling",
         Description: "Unsure about your career path? Our career counselors are here to provide expert guidance and advice, helping you make informed decisions about your professional journey. From exploring new industries to advancing within your current field, we'll help you navigate your options with clarity and purpose."
     },
     {
+        Image: employment,
         Service: "Employer Services",
         Description: "For businesses seeking top talent, we offer tailored recruitment solutions to meet your hiring needs. From sourcing candidates to conducting screenings and assessments, we'll streamline your recruitment process and deliver exceptional results."
     }
--- src/Pages/Home/Sections/Service.jsx
+++ src/Pages/Home/Sections/Service.jsx
@@ -13,7 +13,12 @@ const Service = () => {
                     Services.map((service, index) => {
                         return (
                             <div key={index} className="card w-[400px] p-4 shadow-lg rounded flex flex-col gap-4 hover:scale-100 hover:shadow-slate-600 ">
-                                <h2 className="text-xl font-bold">{service.Service}</h2>
+                                <div className="flex gap-2 items-center ">
+                                    <div className=" border border-blue-500 rounded-full p-2">
+                                        <img width={25} height={25} src={service.Image} alt="" srcset="" />
+                                    </div>
+                                    <h2 className="text-xl font-bold">{service.Service}</h2>
+                                </div>
                                 <p className="text-lg">{service.Description}</p>
                             </div>
                         )
