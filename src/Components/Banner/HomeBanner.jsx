02c3f32592efa45d62e4164d77840e479b7763e8
octocatblain <blainmuema@gmail.com>
2024-02-24 22:17:10 +0300

    fix: remove direction prop

--- src/Components/Banner/HomeBanner.jsx
+++ src/Components/Banner/HomeBanner.jsx
@@ -6,16 +6,14 @@ import Marquee from "react-fast-marquee";
 const HomeBanner = () => {
     return (
         <Marquee 
-        className="w-full h-[10dvh]  flex flex-row gap-8 items-center justify-center"
+        className="w-full h-[10dvh]  flex flex-row items-center justify-center"
         pauseOnHover={true}
         autoFill={true}
-        direction='right'
-        speed={30}
         >
             {
                 Companies.map((company, index) => {
                     return (
-                        <div key={index} className="w-full h-20">
+                        <div key={index} className="w-full h-20 px-4">
                             <img src={company.image} alt={company.name} title={company.name} className="w-full h-20 object-contain object-center" />
                         </div>
                     )
