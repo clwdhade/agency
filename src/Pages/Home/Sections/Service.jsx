cb27f086ba69af21d87748d36376cb30723b10b1
octocatblain <blainmuema@gmail.com>
2024-02-24 17:17:53 +0300

    fix: adjust bg gradient

--- src/Pages/Home/Sections/Service.jsx
+++ src/Pages/Home/Sections/Service.jsx
@@ -6,13 +6,13 @@ import { Services } from '../../../Data/DataIndex'
 const Service = () => {
 
     return (
-        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 py-8 rounded-xl">
+        <div className="bg-gradient-to-r from-slate-100 to-slate-200 py-8 rounded-lg">
             <h2 className="text-4xl text-center py-8 font-bold">Our <span className="font-light">Services</span></h2>
             <div className="flex flex-wrap gap-12 row-gap-12 justify-center">
                 {
                     Services.map((service, index) => {
                         return (
-                            <div key={index} className="card w-[450px] p-4 shadow-lg rounded flex flex-col gap-4 hover:scale-100 hover:shadow-slate-600 ">
+                            <div key={index} className="card w-[400px] p-4 shadow-lg rounded flex flex-col gap-4 hover:scale-100 hover:shadow-slate-600 ">
                                 <h2 className="text-xl font-bold">{service.Service}</h2>
                                 <p className="text-lg">{service.Description}</p>
                             </div>
