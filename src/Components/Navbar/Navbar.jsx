0de8b5842626248b9bbf9379e087d24901732e66
octocatblain <blainmuema@gmail.com>
2024-02-24 16:10:19 +0300

    fix: make navabr fixed on top

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <navbar className="flex flex-row justify-between px-12 py-4 bg-slate-300  ">
+        <div className="flex flex-row justify-between px-12 py-4 bg-slate-300 fixed top-0 w-full z-50 ">
             <div className="logo text-2xl font-extrabold">Agency</div>
             <ul className='flex flex-row gap-4 text-xl'>
                 <li className='hover:underline-offset-2 hover:underline cursor-pointer' >Home</li >
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <li className='hover:underline-offset-2 hover:underline cursor-pointer' >Contact</li>
 
             </ul>
-        </navbar>
+        </div>
     )
 }
 
