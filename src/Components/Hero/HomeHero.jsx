112bf998c9965d5ada6f8023204a43ddaa44775b
octocatblain <blainmuema@gmail.com>
2024-02-25 23:19:31 +0300

    feat: add mobile responsiveness to hero

--- src/Components/Hero/HomeHero.jsx
+++ src/Components/Hero/HomeHero.jsx
@@ -41,10 +41,10 @@ const HomeHero = () => {
                         return (
                             <SwiperSlide key={index} className='h-dvh w-full text-center'>
                                 <img className='h-full w-full object-cover object-center brightness-50 relative z-0' src={slide.image} alt={slide.title} />
-                                <div className="absolute top-[40%] left-[25dvw] w-[50%] text-white items-center flex flex-col gap-8 z-1">
+                                [<div className="absolute top-[25%] md:top-[40%] left-[12dvw] md:left-[25dvw] w-[75%] md:w-[50%] text-white items-center flex flex-col gap-8 z-1">
                                     <h2 className="text-4xl font-bold">{slide.title}</h2>
                                     <p className='text-xl'>{slide.text}</p>
-                                    <button className="w-[10dvw] flex flex-row gap-2 justify-center items-center py-2 rounded  text-white text-base cursor-pointer border bg-transparent hover:border hover:border-[#072e33] hover:bg-[#072e33] hover:translate-x-2 transition-all">Get Started <BsBoxArrowInUpRight /></button>
+                                    <button className=" w-[75%] md:w-[10dvw] flex flex-row gap-2 justify-center items-center py-2 rounded  text-white text-base cursor-pointer border bg-transparent hover:border hover:border-[#072e33] hover:bg-[#072e33] hover:translate-x-2 transition-all">Get Started <BsBoxArrowInUpRight /></button>
                                 </div>
                             </SwiperSlide>
                         )
