5510e5a2fe32fc35b0a4cb925f07a0efe826288b
octocatblain <blainmuema@gmail.com>
2024-02-29 16:25:53 +0300

    fix: add transition

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   const closeNavbar = () => {
-    setIsResponsive(false); // Close the navbar
+    setIsResponsive(false); 
   };
 
   return (
@@ -21,28 +21,28 @@ const Navbar = () => {
         <span className="text-yellow-500">A</span>gency
       </div>
       <ul className={`nav_menu md:flex ${responsive ? 'flex flex-col gap-4 h-[50dvh] transition-all ease-in-out justify-center items-center' : 'flex-row gap-4'} text-xl ${responsive ? '' : 'hidden'}`}>
-        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500" onClick={closeNavbar}>
+        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500 transition-all ease-in-out" onClick={closeNavbar}>
           <Link to="/">Home</Link>
         </li>
-        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500" onClick={closeNavbar}>
+        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500 transition-all ease-in-out" onClick={closeNavbar}>
           <Link to="/services">Services</Link>
         </li>
-        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500" onClick={closeNavbar}>
+        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500 transition-all ease-in-out" onClick={closeNavbar}>
           <Link to="/about">About</Link>
         </li>
-        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500" onClick={closeNavbar}>
+        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500 transition-all ease-in-out" onClick={closeNavbar}>
           <Link to="/companies">Companies</Link>
         </li>
-        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500" onClick={closeNavbar}>
+        <li className="hover:underline-offset-2 hover:underline cursor-pointer decoration-yellow-500 transition-all ease-in-out" onClick={closeNavbar}>
           <Link to="/contact">Contact</Link>
         </li>
         <BsXCircle
-          className={`cursor-pointer text-2xl hover:red-400 md:hidden ${responsive ? 'block' : 'hidden'}`}
+          className={`cursor-pointer text-2xl hover:red-400 md:hidden transition-all ease-in-out ${responsive ? 'block' : 'hidden'}`}
           onClick={toggleResponsive}
         />
       </ul>
       <FaBars
-        className={`cursor-pointer text-xl md:hidden ${responsive ? 'hidden' : 'block'}`}
+        className={`cursor-pointer text-xl md:hidden transition-all ease-in-out ${responsive ? 'hidden' : 'block'}`}
         onClick={toggleResponsive}
       />
     </div>
